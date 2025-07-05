1cfdd00e87285600d0f4f0f3cb867ad1df529914
Dhawal <dhawal345@gmail.com>
2025-07-05 09:30:02 +0530

    Added task1 and task2

---
+++ task_2.py
@@ -0,0 +1,8 @@
+f=input('Enter the first name: ')
+l=input('Enter the last name: ')
+greet = '! welcome to the python program.'
+fullname = f + ' ' + l
+
+msg = fullname + greet
+
+print('Hello, ',msg)
